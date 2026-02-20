abbcda1b8be7a0ae33c88877ece3e44afd809f91
Jamie <jasm1@st-andrews.ac.uk>
2026-02-20 17:19:30 +0000

    DIMACS implemented into main

--- src/DPLLMain.c
+++ src/DPLLMain.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "DPLL.h"
 #include "DIMACS.h"
@@ -14,8 +15,22 @@
  * Note: as mentioned in the handout, your main function should take the filename of the DIMACS file as the first command-line argument in argv below.
  */
 int main(int argc, char *argv[]) {
+    // check for no args
+    if (argc == 1)
+    {
+        printf("No arguments provided\nCorrect usage: './DPLLMain {filename}'");
+        exit(1);
+    }
+
     // process the file argument
     DIMACS dimacs = DIMACS_open(argv[1]);
+
+    // check that the file has been able to open
+    if (!dimacs.stream)
+    {
+        printf("Failed to read the file provided. Check that '%s' exists.\nCorrect usage: './DPLLMain {filename}'", argv[1]);
+        exit(1);
+    }
 
     // read the clauses
     ClauseSet clauseSet = new_ClauseSet();
